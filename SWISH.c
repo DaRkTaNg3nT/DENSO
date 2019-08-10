36c5f69abb340c19733940b6bf77ffd790a49e8d
DaRkTaNg3nt <52654927+DaRkTaNg3nt@users.noreply.github.com>
2019-08-10 14:05:28 +0530

    Create swish.c

---
+++ SWISH.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int price, discount, final_price=0,discount_price;
+    scanf("%d %d",&price,&discount);
+    final_price = price;
+    
+    
+    while(price>9){
+        discount_price =(price*discount)/100;
+        final_price = (final_price) + (price - discount_price);
+        price = price - discount_price;
+    }
+    
+    printf("%d",final_price);
+    return 0;
+}
